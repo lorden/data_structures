e9fc1404528f5e7bc6edd3110360a66380abe221
Diego Lorden <djlorden@gmail.com>
2013-07-19 10:58:17 -0700

    Linked List in Python

---
+++ python/LinkedList.py
@@ -0,0 +1,38 @@
+class Node(object):
+    value = None
+    next = None
+
+    def __init__(self, value):
+        self.value = value
+
+
+class LinkedList(object):
+    root = None
+
+    def append(self, node):
+        if not self.root:
+            self.root = node
+        else:
+            parent = self.root
+            while parent.next:
+                parent = parent.next
+            parent.next = node
+
+    def __repr__(self):
+        nodes = []
+        node = self.root
+        while node:
+            nodes.append(str(node.value))
+            node = node.next
+        return '<Linked List: [%s]>' % ' -> '.join(nodes)
+
+
+### Test
+#if __name__ == '__main__':
+#    ll = LinkedList()
+#    ll.append(Node(1))
+#    ll.append(Node(2))
+#    ll.append(Node(3))
+#    ll.append(Node(4))
+#    ll.append(Node(5))
+#    print ll
